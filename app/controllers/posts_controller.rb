3728641dedf46db904f24f005467059db275429e
navpreetsingh <navpreet.singh.k@gmail.com>
2013-10-21 16:41:06 +0530

    Final output

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
   
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+ 
+  def index
+    @posts = Post.all
+  end
+  
   def new
     @post = Post.new
   end
